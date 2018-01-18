ad2ee5cbc3dc45ac50b12375fe70f1049c34c1d4
Clayton Coelho <claytongc@hotmail.com>
2018-01-18 21:19:35 -0200

    new

--- wireshark.py
+++ wireshark.py
@@ -6,44 +6,47 @@ cap = pyshark.FileCapture('gprs1201rjo.pcap', display_filter='data.data and tcp.
 #pkt = cap[24]
 
 def wireshark_data_iso8583(pkt):
-    protocol = pkt.transport_layer
-    src_ip = pkt.ip.src
-    dst_ip = pkt.ip.dst
-    src_port = pkt[pkt.transport_layer].srcport
-    dst_port = pkt[pkt.transport_layer].dstport
-    stream = pkt[pkt.transport_layer].stream
+
     data = pkt.data.data
+    resultado = {
+        "Stream": pkt[pkt.transport_layer].stream,
+        "Protocol": pkt.transport_layer,
+        "IP Origem": pkt.ip.src,
+        "IP Destino": pkt.ip.dst,
+        "Porta Origem": pkt[pkt.transport_layer].srcport,
+        "Porta Destino": pkt[pkt.transport_layer].dstport,
+        "Tamanho Mensagem ISO": int(data[:4], 16),
+        "NII Mensagem ISO": data[6:14],
+        "Tipo Mensagem ISO": data[16:20]
+    }
     x = 0
-    print("Stream: %s" % stream)
-    print("Protocol: %s" % protocol)
-    print("IP Origem: %s" % src_ip)
-    print("IP Destino: %s" % dst_ip)
-    print("Porta Origem: %s" % src_port)
-    print("Porta Destino: %s" %dst_port)
-    print("Tamanho da mensagem ISO: %d" % int(data[:4], 16))
-    print("NII da mensagem ISO: %s" % data[6:14])
-    print("Tipo da mensagem ISO: %s" % data[16:20])
+    y = 1
     bit47_true = False
     while x < len(data):
         if data[x:x + 8] == "ffff0100" and data[x + 10:x + 22] == "303935303730":
             bit47 = bytearray.fromhex(data[x + 24:x + 28]).decode()
-            print("Contém Bit47!")
-            print("Valor do Bit47: %s "% bit47)
+            resultado["%d Valor BIT47" % y] = bit47
             bit47_true = True
-            break
+            y += 1
         x += 1
-    if bit47_true == False:
-        print("Não contém bit47!")
+    resultado["BIT47"] = bit47_true
+    return resultado
+
+bit_sim = 0
+bit_nao = 0
+for pkt in cap:
+
+    r = wireshark_data_iso8583(pkt)
+    if r["BIT47"]:
+        bit_sim += 1
     else:
-        return bit47
-    print("#############################################################################")
-    print()
+        bit_nao += 1
+print("Quantos BIT47: %d" % bit_sim)
+print("Sem BIT47: %d" % bit_nao)
 
 
-#for pkt in cap:
-#    r = wireshark_data_iso8583(pkt)
+#r = wireshark_data_iso8583(cap[0])
+#print(r)
 
-r = wireshark_data_iso8583(cap[0])
-if r == "None":
-    print("Meus ovos")
+
 
